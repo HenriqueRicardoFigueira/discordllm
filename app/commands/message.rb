548bce9be9534b28bfc64b7bc4b0d5257f8c3e44
HenriqueRicardoFigueira <henrislip@gmail.com>
2024-03-20 18:19:47 -0300

    add bot talk to olllma

--- app/commands/message.rb
+++ app/commands/message.rb
@@ -1,29 +1,24 @@
 # frozen_string_literal: true
 
+require 'ollama-ai'
+
 # app/commands/message.rb
 module Message
   extend Discordrb::Commands::CommandContainer
 
   Bot.command(:message) do |event|
-    response = event.message.content[8..]
-    event.respond "Você digitou: #{response}"
+    query = event.message.content[8..]
 
-    response = HTTParty.post('https://api.openai.com/v1/chat/completions', {
-                               headers: {
-                                 'Authorization' => 'Bearer SEU_TOKEN_DO_CHATGPT_AQUI',
-                                 'Content-Type' => 'application/json'
-                               },
-                               body: {
-                                 model: 'gpt-3.5-turbo',
-                                 messages: [
-                                   {
-                                     role: 'system',
-                                     content: "Usuário: #{event.message.content}"
-                                   }
-                                 ]
-                               }.to_json
-                             })
+    client = Ollama.new(
+      credentials: { address: 'http://localhost:11434' },
+      options: { server_sent_events: true }
+    )
 
-    event.respond response['choices'][0]['message']['content']
+    response = client.generate(
+      { model: 'llama2',
+        prompt: query,
+        stream: false }
+    )
+    event.respond response.first['response']
   end
 end
